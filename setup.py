23a472cc90b02adaa28e5688e6b3850314860370
Dylan Whichard <dylan@whichard.com>
2015-08-28 00:01:13 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,52 @@
+from distutils.core import setup
+
+setup(
+    name='pyalexa',
+    packages=['pyalexa'],
+    version="0.1.0",
+    description="Python API for writing new Amazon Echo skills",
+    long_description="""I should write this
+    """,
+    license="""The MIT License (MIT)
+
+    Copyright (c) 2015 Hackafe
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+""",
+    author='Dylan Whichard',
+    author_email='dylan@whichard.com',
+    url='https://github.com/umbc-hackafe/pyalexa',
+    keywords=[
+        'alexa', 'amazon', 'echo', 'flask'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English',
+        'Topic :: Multimedia :: Sound/Audio :: Speech',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
